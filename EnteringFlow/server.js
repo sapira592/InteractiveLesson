d3fe2b3af4f09b3c1fd9e069f2b1de3627655399
Sapir Abargil <sapira592@gmail.com>
2016-06-15 18:08:09 +0300

    only service

--- EnteringFlow/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 8080;
-app.use('/',express.static('./public'));
-// app.get('/' , function(req,res){
-//     res.send("OK");
-// });
-app.listen(port);
-console.log('listening..... on port ' + port);
